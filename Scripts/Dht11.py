1be64baddf52791e706e9165ce3438bb65dc419d
mkjkdev <mitchel_king@icloud.com>
2020-01-28 08:46:12 +0000

    Prints to LCD, change import from module dir

--- Scripts/Dht11.py
+++ Scripts/Dht11.py
@@ -1,8 +1,33 @@
+#!/usr/bin/python
+
 import Adafruit_DHT
+import time
 
-sensor = Adafruit_DHT.DHT11
-pin = 4
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+import sys
+sys.path.append("/home/pi/code/Gardenpro/GardenYes/Scripts/modules")
+import lcd1602
 
-print("humidity: {} temperature: {} ".format(humidity, temperature))
+lcd = lcd1602.Adafruit_CharLCD()
+
+def DHT():
+	sensor = Adafruit_DHT.DHT11
+	pin = 4
+	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+	return(temperature, humidity)
+
+def LCD(temperature, humidity):
+	lcd.clear()
+	lcd.message("Temperature : ")
+	lcd.message(str(temperature))
+	time.sleep(2.0)
+	lcd.clear()
+
+	lcd.message("Humidity : ")
+	lcd.message(str(humidity))
+	time.sleep(2.0)
+	lcd.clear()
+
+temperature, humidity = DHT()
+LCD(temperature, humidity)
+
 exit()
